a1c341b08c1dab9eaef0a8a551279037cbf1f3a4
13mtfb <burton.matthew@hotmail.com>
2021-02-20 18:48:37 -0500

    add entries to PST, change additional files

--- Macro_Assembler/PST.cpp
+++ Macro_Assembler/PST.cpp
@@ -3,10 +3,79 @@
 #include "stdafx.h"
 #include "iostream"
 #include "fstream"
+#include <string>
 
 using namespace std;
 
+enum operatorType {
+	SINGLE_OPERAND,
+	DOUBLE_OPERAND,
+	BRANCH,
+	JUMP_SUBROUTINE,
+	TRAP_INTERRUPT,
+	MISCELLANEOUS,
+	CONDITION_CODE,
+};
+
+
+struct opCode {
+	string mnemonic;
+	char opcode;
+	int operatortype;
+	
+};
+
 ////// OP CODES //////
+
+static const opCode instruction[] = {
+	{"HALT",	000000, MISCELLANEOUS},			//halt
+	{"WAIT",	000001, MISCELLANEOUS},			//wait for interrupt
+	{"RTI",		000002, TRAP_INTERRUPT},		//return from interrupt
+	{"BPT",		000003, TRAP_INTERRUPT},		//breakpoint trap
+	{"IOT",		000004, TRAP_INTERRUPT},		//input/output trap
+	{"RESET",	000005, MISCELLANEOUS},			//reset external bus
+	{"RTT",		000006, TRAP_INTERRUPT},		//return from interrupt
+	{"JMP",		000100, JUMP_SUBROUTINE},		//jump
+	{"RTS",		000200, JUMP_SUBROUTINE},		//return from subroutine
+	{"SPL",		000230, MISCELLANEOUS},			//set priority level
+	{"NOP",		000240, MISCELLANEOUS},			//no operation
+	{"CLC",		000241, CONDITION_CODE},		//clear C
+	{"CLV",		000242, CONDITION_CODE },		//clear V
+	{"CLZ",		000244, CONDITION_CODE },		//clear Z
+	{"CLN",		000250, CONDITION_CODE },		//clear N
+	{"CCC",		000257, CONDITION_CODE },		//clear all cc bits
+	{"SEC",		000261, CONDITION_CODE },		//set C
+	{"SEV",		000262, CONDITION_CODE },		//set V
+	{"SEZ",		000264, CONDITION_CODE },		//set Z
+	{"SEN",		000270, CONDITION_CODE },		//set N
+	{"SCC",		000277, CONDITION_CODE },		//set all cc bits
+	{"SWAB",	000300, SINGLE_OPERAND},		//swap bytes
+	{"BR",		000400, BRANCH},				//branch (unconditional)
+	{"BNE",		001000, BRANCH},				//br if not equal (to 0)
+	{"BEQ",		001400, BRANCH},				//br if equal (to 0)
+	{"BGE",		002000, BRANCH},				//br if greater or eq (to 0)
+	{"BLT",		002400, BRANCH},				//br if less than (0)
+	{"BGT",		003000, BRANCH},				//br if greater than (0)
+	{"BLE",		003400, BRANCH},				//br if less or equal to (0)
+	{"JSR",		004000, JUMP_SUBROUTINE},		//jump to subroutine
+	{"CLR",		005000, SINGLE_OPERAND},		//clear
+	{"COM",		005100,	SINGLE_OPERAND},		//complement (1's)
+	{"INC",		005200, SINGLE_OPERAND},		//increment
+	{"DEC",		005300, SINGLE_OPERAND},		//decrement
+	{"NEG",		005400,	SINGLE_OPERAND},		//negate (2's)
+	{"ADC",		005500, SINGLE_OPERAND},		//add carry
+	{"SBC",		005600, SINGLE_OPERAND},		//subtract carry
+	{"TST",		005700, SINGLE_OPERAND},		//test
+	{"ROR",		006000, SINGLE_OPERAND},		//rotate right
+	{"ROL",		006100, SINGLE_OPERAND},		//rotate left
+	{"ASR",		006200, SINGLE_OPERAND},		//arith shift right
+	{"ASL",		006300, SINGLE_OPERAND},		//arith shift left
+	{"MARK",	006400, JUMP_SUBROUTINE},		//mark
+	{"MFPI",	006500, MISCELLANEOUS},			//move from previous instr space
+	{"MTPI",	006600, MISCELLANEOUS},			//move to previous instr space
+	{"SXT",		006700, SINGLE_OPERAND},		//sign extend
+};
+
 
 char register_symbols[] = {	'R0',	// register 0 definition
 							'R1',	// register 1 definition
@@ -19,49 +88,6 @@ char register_symbols[] = {	'R0',	// register 0 definition
 							'\0'	// end of array
 };
 
-
-////// OP CODES //////
-						//	menmonic   name				opcode
-char single_operand[] = {	'CLR',	// clear			X050DD
-							'COM',	// complement (1's)	X051DD
-							'INC',	// increment		X052DD
-							'DEC',	// decrement		X053DD
-							'NEG',	// negate (2's)		X054DD
-							'TST',	// test				X057DD
-							'ROR',	// rotate right		X060DD
-							'ROL',	// rotate left		X061DD
-							'ASR',	// arith shift R	X062DD
-							'ASL',	// arith shift L	X063DD
-							'SWAB',	// swap bytes		0003DD
-							'ADC',	// add carry		X055DD
-							'COM',	// subtract carry	X056DD
-							'STX',	// sign extend		0067DD
-							'\0'	// end of array
-};
-
-char double_operand[] = {
-							'\0'	// end of array
-};
-
-char branch[] = {
-							'\0'	// end of array
-};
-
-char jump_subroutine[] = {
-							'\0'	// end of array
-};
-
-char trap_interrupt[] = {
-							'\0'	// end of array
-};
-
-char misellaneous[] = {
-							'\0'	// end of array
-};
-
-char condition_code[] = {
-							'\0'	// end of array
-};
 
 ////// Macro-11 Directives //////
 
--- Macro_Assembler/scanner.h
+++ Macro_Assembler/scanner.h
@@ -2,20 +2,24 @@
 #include "sstream"
 #include "fstream"
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class scanner {
 public:
-	scanner(string filepath);			// create the filestream using the filename passed to the object
-	int scan();					// returns next token in filestream
+	scanner(string filepath);				// create the filestream using the filename passed to the object
+	int scan();								// returns next token in filestream
+	vector<string> returnCompoundTokens();	// returns vector of compound tokens
+	void clearCompoundTokens();				// clears vector
 private:
-	fstream file;						// file stream
-	string current_line;				// string to hold current line
-	char current_char;					// char to hold current char
-	int current_position;
+	fstream file;							// file stream
+	string current_line;					// string to hold current line
+	char current_char;						// char to hold current char
+	unsigned int current_position;
 	bool EOF_flag;
 	string buffer;
+	vector<string> compoundTokens;
 	int LocationOrSymbol();
 	int NumericLiteralOrSymbol();
 	int Symbol();
